ff5352e7ee632f98247bc0f79da1fe1c7eb980ae
DilyaShakirova <shakirovadilya@yandex.ru>
2024-10-19 19:58:55 +0300

    add program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int amount = 154, replenishment = 1001, bonus = 0;
+        int finalAmount;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+            finalAmount = amount + replenishment + bonus;
+        } else {
+            finalAmount = amount + replenishment;
+        }
+        System.out.println("Количество зачисленных бонусов: " + bonus + " руб.");
+        System.out.println("Сумма на счету с учётом бонусов: " + finalAmount + " руб.");
     }
 }
